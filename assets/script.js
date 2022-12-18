0b5c7132467fdfc7074c8650c493b1d433ea08fb
JGJ <jordan.gb0@gmail.com>
2022-12-18 00:56:00 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,29 @@
 var timeEl = document.querySelector("#timer");
 var starButton = document.querySelector;
 ("#button");
-var h1Element = document.getElementById("h1ElementId");
-h1Element.innerHTML = firstQuestion;
 // Variables
 //
 // h1 Variables/Questions
-var firstQuestion = "Whats is the largest h1 element?";
-var secondQuestion ="What css property changes the color of words?"
-var thirdQuestiion = "What is the git code to change directories?"
-var fourthQuestion = " What CSS property rounds images?"
-var fifthQuestion = "What is the name of the language that interacts with web pages?" 
+var i = 0;
+var h1Element = document.getElementById("#h1ElementId");
+h1Element.innerHTML = firstQuestion;
+var firstQuestion = [
+  "Whats is the largest h1 element?",
+  "What css property changes the color of words?",
+  "What is the git code to change directories?",
+  "What CSS property rounds images?",
+  "What is the name of the language that interacts with web pages?",
+];
 
+function incrementNumber() {
+  h1Element.innerHTML = firstQuestion[i];
+}
 // h1 Variable
 //
 //
 //This will also start the timer
 //Start the Game
-function startGame() {
-     var gameTimer= setInterval (function() {
-} ,1000)
+
 //Change the h1 by the click of the button
 // Attach event listener to increment button element
 startButton.addEventListener("click", startGame);
-
-    
-});
