0c3b0b4cadb23a059aff57db5a54317a9ce609b4
JGJ <jordan.gb0@gmail.com>
2022-12-17 23:25:45 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,28 @@
 // Variables
 var starButton = document.querySelector;
 ("#button");
+var h1Element = document.getElementById("h1ElementId");
+h1Element.innerHTML = firstQuestion;
 // Variables
 //
-// h1 Variables
+// h1 Variables/Questions
 var firstQuestion = "Whats is the largest h1 element?";
+var secondQuestion ="What css property changes the color of words?"
+var thirdQuestiion = "What is the git code to change directories?"
+var fourthQuestion = " What CSS property rounds images?"
+var fifthQuestion = "What is the name of the language that interacts with web pages?" 
+
 // h1 Variable
 //
 //
+//This will also start the timer
 //Start the Game
 function startGame() {
   startButton.textContent = h1Questions;
 }
+//Change the h1 by the click of the button
 // Attach event listener to increment button element
-starButton.addEventListener("click", function () {
-  h1Questions = [0];
-  setCounterText();
+firstQuestion.addEventListener("click", function () {} {
+    
+startGame();
 });
