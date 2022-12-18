40252d85e0fcca282827d24cdcacc45401a29ece
JGJ <jordan.gb0@gmail.com>
2022-12-17 21:53:20 -0500

    beginning first api change with h1 as array of questions

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// Variables
+var starButton = document.querySelector;
+("#button");
+// Variables
+//
+//
+//
+// h1 Variables
+var h1Questions =
+  "Whats is the largest h1 element ? , What CSS element do you use to change the color of text?.";
+var h1QuestionsList = h1Questions.split(" ");
+// h1 Variable
+//
+//
+//
+function startGame() {
+  startButton.textContent = h1Questions;
+}
+// Attach event listener to increment button element
+starButton.addEventListener("click", function () {
+  h1Questions = [0];
+  setCounterText();
+});
