80f1614d5932468cb525a497b7081a4b34a2d254
JGJ <jordan.gb0@gmail.com>
2022-12-17 22:27:07 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,12 @@ var starButton = document.querySelector;
 ("#button");
 // Variables
 //
-//
-//
 // h1 Variables
-var h1Questions =
-  "Whats is the largest h1 element ? , What CSS element do you use to change the color of text?.";
-var h1QuestionsList = h1Questions.split(" ");
+var firstQuestion = "Whats is the largest h1 element?";
 // h1 Variable
 //
 //
-//
+//Start the Game
 function startGame() {
   startButton.textContent = h1Questions;
 }
