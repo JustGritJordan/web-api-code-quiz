40ff71d1afcbc28c0b08b311fb42da5d216cde44
JGJ <jordan.gb0@gmail.com>
2022-12-19 19:54:46 -0500

    code as of 12/19/22

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,19 @@
 // Variables
 var timeEl = document.querySelector("#timer");
-var starButton = document.querySelector;
-("#button");
+var startButton = document.querySelector("#button");
+var answerSelections = document.querySelector("p-section");
 // Variables
 //
 // h1 Variables/Questions
 var i = 0;
-var h1Element = document.getElementById("#h1ElementId");
-h1Element.innerHTML = firstQuestion;
+var h1Element = document.getElementById("h1ElementId");
+// var questions = [
+//   {
+// title: "Commonly used data types DO NOT include:",
+// choices: ["strings", "booleans", "alerts", "numbers"],
+// answer: "alerts"
+//   },
+
 var firstQuestion = [
   "Whats is the largest h1 element?",
   "What css property changes the color of words?",
@@ -15,16 +21,16 @@ var firstQuestion = [
   "What CSS property rounds images?",
   "What is the name of the language that interacts with web pages?",
 ];
-
-function incrementNumber() {
+var answerButtons = document.createElement("button");
+set.a;
+// h1 Variable/Questions
+function startGame() {
   h1Element.innerHTML = firstQuestion[i];
+  i++;
+  console.log("firstQuestion");
 }
-// h1 Variable
-//
-//
-//This will also start the timer
-//Start the Game
 
-//Change the h1 by the click of the button
-// Attach event listener to increment button element
 startButton.addEventListener("click", startGame);
+// hide this div
+
+// change the p in html to ol list of buttons
