4972563a3fd068581fa3e31225b08abd3ec5e1ac
JGJ <jordan.gb0@gmail.com>
2023-01-07 21:13:40 -0500

    final of README

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 ## Description
 
-The purpose of this project is to solidify API concept. The purpose of this project is to use the facet of selection and API maniplulation for over uses
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+
+My motivation for this project was to learn how to execute multiple functions and event listners to better understand the use of api concepts. I learned how much time it takes to learn then implement concepts when only seeing them one time. Troubleshooting is a long and lengthy process, everything steps ususaly take longer than expected. I also learned the the better I prepare for my tutor sessions that more I can learn in an hour. Also and hour in code time is about 20 minutes in the real world.
 
 ## License
 
 Liscense.MIT
+
+## Links
+
+## Screenshots
+
+![code-quiz-challenge](./assets/screenshots/coding-quiz.png)
--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,78 @@
-// Variables
 var timeEl = document.querySelector("#timer");
 var startButton = document.querySelector("#button");
 var answerSelections = document.querySelector("p-section");
-// Variables
-//
-// h1 Variables/Questions
+var time = 45;
+var timer;
 var i = 0;
 var h1Element = document.getElementById("h1ElementId");
-// var questions = [
-//   {
-// title: "Commonly used data types DO NOT include:",
-// choices: ["strings", "booleans", "alerts", "numbers"],
-// answer: "alerts"
-//   },
-
-var firstQuestion = [
-  "Whats is the largest h1 element?",
-  "What css property changes the color of words?",
-  "What is the git code to change directories?",
-  "What CSS property rounds images?",
-  "What is the name of the language that interacts with web pages?",
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "Whats is the largest title element?",
+    choices: ["h5", "h2", "h1", "h3"],
+    answer: "h1",
+  },
+  {
+    title: "What css property changes the color of words?",
+    choices: ["text-size", "text-font", "text-color", "background color"],
+    answer: "alerts",
+  },
+  {
+    title: "What is the git code to change directories?",
+    choices: ["git checkout", "cd", "mkdir", "git pull"],
+    answer: "cd",
+  },
+  {
+    title: "What CSS property rounds images?",
+    choices: ["strings", "br", "padding", "border-radius"],
+    answer: "border-radius",
+  },
 ];
+
 var answerButtons = document.createElement("button");
-set.a;
-// h1 Variable/Questions
+
 function startGame() {
-  h1Element.innerHTML = firstQuestion[i];
-  i++;
-  console.log("firstQuestion");
+  timer = setInterval(function () {
+    time--;
+    timeEl.textContent = time;
+    if (time == 0) {
+      gameOver();
+    }
+  }, 1000);
+
+  displayQuestion();
+}
+
+function displayQuestion() {
+  document.getElementById("question-container").innerHTML = "";
+  document.getElementById("question-container").innerHTML = questions[i].title;
+  for (var j = 0; j < questions[i].choices.length; j++) {
+    var button = document.createElement("button");
+    button.addEventListener("click", function (event) {
+      var choice = event.target.innerHTML;
+      if (questions[i].answer != choice) {
+        time -= 10;
+      }
+      if (questions.length - 1 == i) {
+        gameOver();
+      } else {
+        i += 1;
+        displayQuestion();
+      }
+    });
+    button.innerHTML = questions[i].choices[j];
+    document.getElementById("question-container").append(button);
+  }
+}
+
+function gameOver() {
+  clearInterval(timer);
+  document.getElementsByClassName("quiz-container")[0].innerHTML = "";
+  prompt("What are your intials?");
 }
 
 startButton.addEventListener("click", startGame);
-// hide this div
-
-// change the p in html to ol list of buttons
